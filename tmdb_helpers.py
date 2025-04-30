86b2bb7a2d275b8256c30383ea8c65220ce2a4b2
olenevoid <arnafas@gmail.com>
2025-04-30 15:52:02 +0500

    Add proxy for requests

--- tmdb_helpers.py
+++ tmdb_helpers.py
@@ -1,11 +1,11 @@
-import urllib.request
-import urllib.parse
-import json
+import requests
 from getpass import getpass
+import os
+
 
 def make_tmdb_api_request(method, api_key, extra_params=None):
     extra_params = extra_params or {}
-    url = 'https://api.themoviedb.org/3%s' % method
+    url = 'http://api.themoviedb.org/3%s' % method
     params = {
         'api_key': api_key,
         'language': 'ru',
@@ -13,19 +13,34 @@ def make_tmdb_api_request(method, api_key, extra_params=None):
     params.update(extra_params)
     return load_json_data_from_url(url, params)
 
+
 def load_json_data_from_url(base_url, url_params):
-    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
-    response = urllib.request.urlopen(url).read().decode('utf-8')
-    return json.loads(response)
+    proxy = get_proxy()
+    proxies = {}
+    
+    if proxy is not None:
+        proxies['http'] = 'http://%s' % get_proxy()
+    
+    response = requests.get(base_url, params=url_params, proxies=proxies)
+    return response.json()
+
 
 def get_user_api_key():
     user_api_key = getpass('Enter your api key v3:')
     try:
         make_tmdb_api_request(method='/movie/2', api_key = user_api_key)
         return user_api_key
-    except urllib.error.HTTPError as err:
+    except requests.exceptions.HTTPError as err:
         if err.code == 401:
             return None
         else:
             raise
 
+
+def get_proxy():
+    proxy_file = 'proxy.txt'
+    if os.path.exists(proxy_file):
+        with open(proxy_file, 'r') as file:
+            return file.readline()
+        
+    return None
